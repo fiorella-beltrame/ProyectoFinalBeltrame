512ef6234616f5a5ca5a7cbc866b186aaf396aad
Fiorella Beltrame <fio.beltrame20@gmail.com>
2025-07-27 23:34:21 -0300

    CreaTuLanding1+Beltrame

---
+++ src/components/itemlistcontainer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemListContainer = ({saludo}) => {
+    return(
+        <section style={{padding: '2rem'}}>
+            <h2>{saludo}</h2>
+        </section>
+    );
+};
+
+export default ItemListContainer;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import CartWidget from '../cartwidget';
+import logo from '../../img/logo.png';
+
+const NavBar = () => {
+
+    const [cartCount, setCartCount] = useState (0);
+    const addItemToCart = () => {
+        setCartCount(cartCount+1);
+    }
+
+    return(
+        <nav style={{display: 'flex', justifyContent: 'space-between', padding: '1rem', background: '#eee', alignItems: 'center'}}>
+            <div>
+                <img src={logo} alt="Logo de Materia Joyas" style= {{height:'150px' }}></img>
+            </div>
+            <div>
+                <a href="#" style={{color: 'inherit', textDecoration: 'none', margin: '0 1rem'}}>Inicio</a>
+                <a href="#" style={{color: 'inherit', textDecoration: 'none', margin: '0 1rem'}}>Productos</a>
+                <a href="#" style={{color: 'inherit', textDecoration: 'none', margin: '0 1rem'}}>Contacto</a>
+            </div>
+            <CartWidget count = {cartCount} />
+            <button onClick={addItemToCart}>Agregar al carrito</button>
+        </nav>
+    );
+};
+
+export default NavBar;
